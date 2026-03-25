bf77addf5263e8a8ea2e9bca9a58b90f7b3d9167
vikrampatil456 <vikrampatlolaa@gmail.com>
2026-03-25 19:48:05 +0530

    userService implemented

--- src/main/java/com/student/management/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/student/management/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.student.management.Exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.csrf.CsrfException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,15 +12,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(CsrfException.class)
-    public String csrfExceptionHandler(RedirectAttributes redirectAttributes) {
+    public String csrfExceptionHandler(CsrfException ex, RedirectAttributes redirectAttributes) {
+        log.warn("CSRF validation failed", ex.getMessage());
         redirectAttributes.addFlashAttribute("message", "session expired, please login again.");
         return "redirect:/login";
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String genericExceptionHandler() {
+    public String genericExceptionHandler(Exception ex) {
+        log.error("CSRF validation failed", ex.getMessage());
         return "500";
     }
 
---
+++ src/main/java/com/student/management/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.student.management.service;
+
+import com.student.management.Repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
